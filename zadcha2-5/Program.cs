1f4410145df3a769d7dbbefc47ddd33a7f30ff3a
Илюша <ilaskrabin589@gmail.com>
2022-02-17 16:28:18 +0300

    Задача2-5

--- zadcha2-5/Program.cs
+++ zadcha2-5/Program.cs
@@ -7,6 +7,9 @@ namespace zadcha2_5
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int dsfsdf;
+            sddsf
+                dsfsfd
         }
     }
 }
